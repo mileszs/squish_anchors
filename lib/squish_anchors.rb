09b7c3cf2f4975f9fac0ae939af422b3ab4df3c3
Miles Z. Sterrett <miles.sterrett@gmail.com>
2011-04-18 16:21:57 -0400

    Allow single-quoted hrefs, return a string with no matches

--- lib/squish_anchors.rb
+++ lib/squish_anchors.rb
@@ -1,11 +1,15 @@
 module SquishAnchors
   def squish_anchors
     str = self.dup
-    match = /<a\shref="(.*?)">([^<]+)<\/a>/.match(str)
-    while (next_match = /#{match[0]}(\s*)<a\shref="#{match[1]}">([^<]+)<\/a>/.match(str))
-      str.gsub!(/#{match[0]}\s*<a\shref="#{match[1]}">([^<]+)<\/a>/, "<a href=\"#{match[1]}\">#{match[2]}#{next_match[1]}\\1</a>") 
-      match = /<a\shref="(.*?)">([^<]+)<\/a>/.match(str)
+    match = /<a\shref=["|'](.*?)["|']>([^<]+)<\/a>/.match(str)
+    if (match)
+      while (next_match = /#{match[0]}(\s*)<a\shref=["|']#{match[1]}["|']>([^<]+)<\/a>/.match(str))
+        str.gsub!(/#{match[0]}\s*<a\shref=["|']#{match[1]}["|']>([^<]+)<\/a>/, "<a href=\"#{match[1]}\">#{match[2]}#{next_match[1]}\\1</a>") 
+        match = /<a\shref=["|'](.*?)["|']>([^<]+)<\/a>/.match(str)
+      end
+      str.gsub(/\s+/, ' ')
+    else
+      str
     end
-    str.gsub(/\s+/, ' ')
   end
 end
--- test/squish_anchors_test.rb
+++ test/squish_anchors_test.rb
@@ -4,7 +4,6 @@ class String; include SquishAnchors; end
 class SquishAnchorsTest < Test::Unit::TestCase
   def test_squish_anchors_combines_multiple_adjacent_anchor_tags_with_identical_hrefs_in_a_string_into_one
     str = '<a href="/foo/bar"> This is</a>  <a href="/foo/bar">some mal </a> <a href="/foo/bar"> formed HTML content</a> from a JS wysiwig HTML editor'
-    puts str.squish_anchors
     assert str.squish_anchors.scan(/<a\shref="\/foo\/bar">([^<]+)<\/a>/).size == 1
   end
 
@@ -18,5 +17,15 @@ class SquishAnchorsTest < Test::Unit::TestCase
     str = orig_str.dup
     str.squish_anchors
     assert orig_str == str
+  end
+
+  def test_squish_anchors_should_handle_single_quoted_href_values
+    str = "<a href='/foo/bar'> This is</a>  <a href='/foo/bar'>some mal </a> <a href='/foo/bar'> formed HTML content</a> from a JS wysiwig HTML editor"
+    assert str.squish_anchors.scan(/<a\shref="\/foo\/bar">([^<]+)<\/a>/).size == 1
+  end
+
+  def test_squish_anchors_should_return_the_string_if_no_anchors_are_present
+    str = "This string doesn't have any matching anchors"
+    assert str.squish_anchors == str
   end
 end
