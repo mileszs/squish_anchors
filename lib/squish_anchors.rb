f84a2f85ce502a4b44903294f6035ada73f7a6dc
Chris <christopher@fastestforward.com>
2011-04-19 12:35:49 -0400

    collapse regexp to constants, ignore case & multiline

--- lib/squish_anchors.rb
+++ lib/squish_anchors.rb
@@ -1,11 +1,17 @@
 module SquishAnchors
+  LINK_REGEX_BASE = "<a\\shref=[\"']%s[\"']\\s*>([^<]+)<\\/a>"
+  REGEXP_OPTIONS = Regexp::IGNORECASE | Regexp::MULTILINE
+  LINK_REGEX = Regexp.new(LINK_REGEX_BASE % ["(.*?)"], REGEXP_OPTIONS)
+
+           
   def squish_anchors
     str = self.dup
-    match = /<a\shref=["|'](.*?)["|']>([^<]+)<\/a>/.match(str)
+    match = LINK_REGEX.match(str)
     if (match)
-      while (next_match = /#{match[0]}(\s*)<a\shref=["|']#{match[1]}["|']>([^<]+)<\/a>/.match(str))
-        str.gsub!(/#{match[0]}\s*<a\shref=["|']#{match[1]}["|']>([^<]+)<\/a>/, "<a href=\"#{match[1]}\">#{match[2]}#{next_match[1]}\\1</a>") 
-        match = /<a\shref=["|'](.*?)["|']>([^<]+)<\/a>/.match(str)
+      while ((matcher = Regexp.new("%s(\\s*)#{LINK_REGEX_BASE.to_s}" % [match[0], match[1]], REGEXP_OPTIONS)) && 
+              (next_match = matcher.match(str)))
+        str.gsub!(matcher, "<a href=\"#{match[1]}\">#{match[2]}#{next_match[1]}\\1</a>")
+        match = LINK_REGEX.match(str)
       end
       str.gsub(/\s+/, ' ')
     else
